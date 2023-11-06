ce6d20da149ec145652b285dc45faca1478b1e8c
Eric mnz <48776197+mi1048@users.noreply.github.com>
2023-11-06 16:34:37 -0300

    Color convert for mediapipe

--- keypoints.py
+++ keypoints.py
@@ -5,13 +5,25 @@ from matplotlib import pyplot as plt
 import time
 import mediapipe as mp
 
+mp_holistic = mp.solutions.holistic #holistic model
+mp_drawing = mp.solutions.drawing_utils #drawing utilities
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #Color conversion
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow('Opencv Feed', frame)
+    ret, frame = cap.read() #read the feed
+    cv2.imshow('Opencv Feed', frame) #show to screen
 
     if cv2.waitKey(5) & 0xFF == ord('q'):
         break
-cap.release()
+cap.release() 
 cv2.destroyAllWindows()
 
