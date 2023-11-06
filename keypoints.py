5140fe6cf7d6b1a807fa1cb09a861fb4dbb9aca7
Eric mnz <48776197+mi1048@users.noreply.github.com>
2023-11-06 17:06:31 -0300

    showing mediapipe landmarks

--- keypoints.py
+++ keypoints.py
@@ -9,21 +9,36 @@ mp_holistic = mp.solutions.holistic #holistic model
 mp_drawing = mp.solutions.drawing_utils #drawing utilities
 
 def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #Color conversion
-    image.flags.writeable = False
-    results = model.process(image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #Color conversion BGR to RGB
+    image.flags.writeable = False                    
+    results = model.process(image)                 #Make prediction
     image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) #Color conversion
     return image, results
 
+def draw_Landmarks(image, results):
+    #mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.HAND_CONNECTIONS)
+    #mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read() #read the feed
-    cv2.imshow('Opencv Feed', frame) #show to screen
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
 
-    if cv2.waitKey(5) & 0xFF == ord('q'):
-        break
-cap.release() 
-cv2.destroyAllWindows()
+        ret, frame = cap.read() #read the feed
 
+        image, results = mediapipe_detection(frame, holistic)
+        
+        draw_Landmarks(image, results)
+        
+        cv2.imshow('Opencv Feed', image) #show to screen
+
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+    cap.release() 
+    cv2.destroyAllWindows()
+
+draw_Landmarks(frame, results)
+
+plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
